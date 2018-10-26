63b6c8d1eab5b6dbe8341ff36847190bb8f984f3
Fil Lawrence Luzadas <flg.l@outlook.com>
2018-10-26 15:46:47 -0400

    Added ability to add education and experience from profile
    Along with education and experience validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,8 @@ const passport = require("passport");
 
 // Load validation
 const validateProfileInput = require("../../validation/profile");
+const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 
 // Load Models
 const Profile = require("../../models/Profile");
@@ -177,4 +179,81 @@ router.post(
     }
 );
 
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post(
+    "/experience",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateExperienceInput(req.body);
+
+        // Check validation
+        if (!isValid) {
+            // Return errors
+            return res.status(400).json(errors);
+        }
+
+        Profile.findOne({ user: req.user.id }).then(profile => {
+            const newExp = {
+                title: req.body.title,
+                company: req.body.company,
+                location: req.body.location,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            };
+
+            // Add to experience array of currently logged in user, then save
+            // Using unshift to add experience to beginning of array
+            profile.experience.unshift(newExp);
+            profile.save().then(profile => res.json(profile));
+        });
+    }
+);
+
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post(
+    "/education",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateEducationInput(req.body);
+
+        // Check validation
+        if (!isValid) {
+            // Return errors
+            return res.status(400).json(errors);
+        }
+
+        Profile.findOne({ user: req.user.id }).then(profile => {
+            const newEdu = {
+                school: req.body.school,
+                degree: req.body.degree,
+                fieldOfStudy: req.body.fieldOfStudy,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            };
+
+            // Add to education array of currently logged in user, then save
+            profile.education.unshift(newEdu);
+            profile.save().then(profile => res.json(profile));
+        });
+    }
+);
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.post(
+    "/experience/:exp_id",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Profile.findOne({ user: req.user.id }).then(profile => {});
+    }
+);
 module.exports = router;
