b6fc4c527981dd9bd193fad08698db79f449b8dd
Fil Lawrence Luzadas <flg.l@outlook.com>
2018-10-27 01:29:39 -0400

    Set up links to register and login
    Component state implementation, Router, register/login form

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Landing extends Component {
     render() {
@@ -17,18 +18,18 @@ export default class Landing extends Component {
                                     posts and get help from other developers
                                 </p>
                                 <hr />
-                                <a
-                                    href="register.html"
+                                <Link
+                                    to="/register"
                                     className="btn btn-lg btn-info mr-2"
                                 >
                                     Sign Up
-                                </a>
-                                <a
-                                    href="login.html"
+                                </Link>
+                                <Link
+                                    to="/login"
                                     className="btn btn-lg btn-light"
                                 >
                                     Login
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
