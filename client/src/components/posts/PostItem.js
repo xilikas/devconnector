66b524f0296bc52ee06b1c1985d09eb90640417c
Fil Lawrence Luzadas <flg.l@outlook.com>
2018-10-28 19:30:41 -0400

    Like and unlike posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,11 +3,26 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import classnames from "classnames";
-import { deletePost } from "../../actions/postActions";
+import { deletePost, addLike, removeLike } from "../../actions/postActions";
 
 class PostItem extends Component {
     onDeleteClick(id) {
         this.props.deletePost(id);
+    }
+
+    onLikeClick(id) {
+        this.props.addLike(id);
+    }
+
+    onUnlikeClick(id) {
+        this.props.removeLike(id);
+    }
+
+    findUserLike(likes) {
+        const { auth } = this.props;
+        if (likes.filter(like => like.user === auth.user.id).length > 0)
+            return true;
+        else return false;
     }
 
     render() {
@@ -28,13 +43,27 @@ class PostItem extends Component {
                     </div>
                     <div className="col-md-10">
                         <p className="lead">{post.text}</p>
-                        <button type="button" className="btn btn-light mr-1">
-                            <i className="text-info fas fa-thumbs-up" />
+                        <button
+                            onClick={this.onLikeClick.bind(this, post._id)}
+                            type="button"
+                            className="btn btn-light mr-1"
+                        >
+                            <i
+                                className={classnames("fas fa-thumbs-up", {
+                                    // The user liked the post; make thumb green
+                                    // Checks for the user in the post likes array
+                                    "text-info": this.findUserLike(post.likes)
+                                })}
+                            />
                             <span className="badge badge-light">
                                 {post.likes.length}
                             </span>
                         </button>
-                        <button type="button" className="btn btn-light mr-1">
+                        <button
+                            onClick={this.onUnlikeClick.bind(this, post._id)}
+                            type="button"
+                            className="btn btn-light mr-1"
+                        >
                             <i className="text-secondary fas fa-thumbs-down" />
                         </button>
                         <Link
@@ -65,7 +94,9 @@ class PostItem extends Component {
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
-    deletePost: PropTypes.func.isRequired
+    deletePost: PropTypes.func.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -74,5 +105,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    { deletePost }
+    { deletePost, addLike, removeLike }
 )(PostItem);
