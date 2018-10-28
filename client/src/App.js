8c6bfbec02bc3b96f67ac1545697232de3c64c29
Fil Lawrence Luzadas <flg.l@outlook.com>
2018-10-28 02:49:43 -0400

    Edit profile functionality and route

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
 import store from "./store";
 import setAuthToken from "./utils/setAuthToken";
 import { clearCurrentProfile } from "./actions/profileActions";
@@ -64,6 +65,13 @@ class App extends Component {
                                     exact
                                     path="/create-profile"
                                     component={CreateProfile}
+                                />
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/edit-profile"
+                                    component={EditProfile}
                                 />
                             </Switch>
                         </div>
